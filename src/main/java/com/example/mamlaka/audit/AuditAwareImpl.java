c271808ded120610241bda473ba629a22bebd191
Shivere <shawn.shivere@zeraki.co.ke>
2024-09-12 16:08:08 +0300

    Audit trail

--- src/main/java/com/example/mamlaka/audit/AuditAwareImpl.java
+++ src/main/java/com/example/mamlaka/audit/AuditAwareImpl.java
@@ -1,6 +1,9 @@
 package com.example.mamlaka.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -9,13 +12,26 @@ import java.util.Optional;
 public class AuditAwareImpl implements AuditorAware<String> {
 
     /**
-     * Returns the current auditor of the application.
+     * Returns the current auditor of the application (logged-in username).
      *
-     * @return the current auditor.
+     * @return the current auditor (username).
      */
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("SHIVERE_MS");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        // Check if the principal is an instance of UserDetails (i.e., the authenticated user)
+        if (principal instanceof UserDetails) {
+            return Optional.of(((UserDetails) principal).getUsername());
+        } else {
+            // In case the principal is a simple String (e.g., when using token-based authentication)
+            return Optional.of(principal.toString());
+        }
     }
-	
 }
